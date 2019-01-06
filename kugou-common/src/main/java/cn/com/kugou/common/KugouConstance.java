66d4dad8f6c1f9818d8a4443e6f14675e3689241
guhongda <15631973182@163.com>
2019-01-06 14:23:38 +0800

    提交一波，测试git是否正常

---
+++ kugou-common/src/main/java/cn/com/kugou/common/KugouConstance.java
@@ -0,0 +1,17 @@
+package cn.com.kugou.common;
+
+import org.springframework.cloud.stream.messaging.Sink;
+
+/**
+ * @author 黄尚
+ * @brief
+ * @details
+ * @date 2018/10/16
+ */
+public class KugouConstance {
+
+    public static final  String OUTPUT = Sink.INPUT;
+    public static final  String OUTPUT_1 = "input_1";
+    public static final  String OUTPUT_2= "input_2";
+
+}
